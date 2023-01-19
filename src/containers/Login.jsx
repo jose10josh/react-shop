d294ff31cc73d0fd3246206443677afca362e1ec
José Arévalo <joseaf1002@gmail.com>
2023-01-19 15:40:10 -0600

    Add header component

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '@styles/login.scss'
+import '@styles/Login.scss'
 
 class Login extends Component {
   render() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Header from '@components/Header';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>React Shop</h1>
-      </div>
+      <Header/>
     );
   }
 }
