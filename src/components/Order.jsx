76a9027b0b4d2795a40c61a7ed56e18b46c0d311
José Arévalo <joseaf1002@gmail.com>
2023-01-19 16:00:38 -0600

    Create component and styles - order, orderitem, proditem

---
+++ src/components/Order.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "@styles/Order.scss";
+
+class Order extends Component {
+  render() {
+    return (
+      <div className="Order">
+        <p>
+          <span>03.25.21</span>
+          <span>6 articles</span>
+        </p>
+        <p>$560.00</p>
+        <img src="./icons/flechita.svg" alt="arrow" />
+      </div>
+    );
+  }
+}
+
+export default Order;
