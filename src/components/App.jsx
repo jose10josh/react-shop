55b59dd8046cf1cd435418d07268c8c0c89a1c0e
José Arévalo <joseaf1002@gmail.com>
2023-01-19 11:44:10 -0600

    create project webpack config and install loaders and plugins

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../styles/global.scss'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hola mundo</h1>
+        <h1>Hola mundo!!</h1>
       </div>
     );
   }
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  target: "web",
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    watchFiles: path.join(__dirname, './**'),
+  },
+}
