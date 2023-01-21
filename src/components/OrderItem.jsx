0fa1a6006a2fd219c8f0c7b555c810e9f51b3014
José Arévalo <joseaf1002@gmail.com>
2023-01-21 16:06:56 -0600

    Add removefromcart method

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import close from "@icons/icon_close.png";
+import AppContext from '@context/AppContext';
 
 import '@styles/OrderItem.scss';
 
 const OrderItem = ({product}) => {
+  const {removeFromCart} = useContext(AppContext);
+
+  const handleRemove = (item) => {
+    removeFromCart(item);
+  }
+
+
   return (
     <div className="OrderItem">
       <figure>
@@ -15,7 +23,7 @@ const OrderItem = ({product}) => {
       </figure>
       <p>{product.title}</p>
       <p>${product.price}</p>
-      <img src={close} alt="close" />
+      <img src={close} alt="close" onClick={() => handleRemove(item)} />
     </div>
   );
 };
